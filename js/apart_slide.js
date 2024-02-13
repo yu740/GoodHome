d0cb7a2c2657befecb5770e932edd3d62be46141
yu740 <72325298+yu740@users.noreply.github.com>
2024-02-13 22:23:49 +0900

    Add files via upload

---
+++ js/apart_slide.js
@@ -0,0 +1,29 @@
+$('.apart_lineup').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
---
+++ js/slideshow.js
@@ -0,0 +1,11 @@
+﻿$(function(){
+  $("#main_visual li").css({"position":"relative","overflow":"hidden"});
+  $("#main_visual li").hide().css({"position":"absolute","top":0,"left":0});
+  $("#main_visual li:first").addClass("fade").show();
+  setInterval(function(){
+    var $active = $("#mainvisual li.fade");
+    var $next = $active.next("li").length?$active.next("li"):$("#main_visual li:first");
+    $active.fadeOut(2000).removeClass("fade");
+    $next.fadeIn(2000).addClass("fade");
+  },3500);
+});
